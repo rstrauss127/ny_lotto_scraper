75171ecdb31fcf9016d091786036362ef3dabce1
Rebecca Strauss <rstrauss127@gmail.com>
2018-12-02 02:09:07 +0000

    scrape_lotto method can grab name, results, and nextjacpot info

--- lib/ny_lotto_scraper/game.rb
+++ lib/ny_lotto_scraper/game.rb
@@ -1,5 +1,5 @@
 class NyLottoScraper::Game
-  attr_accessor :name, :jackpot, :next_drawing, :latest_winners, :url
+  attr_accessor :name, :jackpot, :next_jackpot, :results, :url
 
   def self.today #scrape each page and then return games based on that data
     self.scrape_games
@@ -7,19 +7,16 @@ class NyLottoScraper::Game
 
   def self.scrape_games
     games = []
-
-    games << self.scrape_mega_millions
-      #Go to mega-millions
-    #extract properties
-    #instantiate a game
-
-    #go to powerball...
+    games << self.scrape_lotto
     games
   end
 
-  def self.scrape_mega_millions
-    doc = Nokogiri::HTML(open("https://nylottery.ny.gov/mega-millions"))
-    name = doc.search("h1.header2").text
+  def self.scrape_lotto
+    doc = Nokogiri::HTML(open("https://www.lotteryusa.com/new-york/"))
+
+    name = doc.css("tbody").css("tr").css("div.game-title").first.text
+    results = doc.css("tbody").css("tr").css("ul.draw-result").first.text
+    nextjackpot = doc.css("tbody").css("tr").css("td.next-jackpot").first.text
     binding.pry
   end
 end
