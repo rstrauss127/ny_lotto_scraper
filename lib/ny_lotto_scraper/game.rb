95f3d57eb412bfa3bd3f8018e5460d765055722c
Rebecca Strauss <rstrauss127@gmail.com>
2018-12-05 22:07:43 +0000

    deleted extra attributes next_draw_date and next_jackpot

--- lib/ny_lotto_scraper/game.rb
+++ lib/ny_lotto_scraper/game.rb
@@ -1,5 +1,5 @@
 class NyLottoScraper::Game
-  attr_accessor :title, :draw_results, :jackpot, :draw_date, :next_draw_date,  :next_jackpot
+  attr_accessor :title, :draw_results, :jackpot, :draw_date
 
 
 
@@ -16,8 +16,6 @@ class NyLottoScraper::Game
           b.draw_results = game.css("ul.draw-result").inner_text.strip.scan(/\d{1,2}/).join(" ")
           b.draw_date = game.css("time").text
           b.jackpot = game.css("td.jackpot").inner_text.strip
-          b.next_draw_date = game.css("td.jackpot").text
-          b.next_jackpot = game.css("span.next-jackpot-amount").text.strip
           games << b
         end
         games.delete_if{|i| i.title == ""}#drop blank objects
