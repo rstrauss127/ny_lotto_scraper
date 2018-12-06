b9e6be2623fecb2a0e262ef511bb87338de8a71e
Rebecca Strauss <rstrauss127@gmail.com>
2018-12-06 23:57:27 +0000

    #add_lottogame enforces type

--- lib/ny_lotto_scraper/state.rb
+++ lib/ny_lotto_scraper/state.rb
@@ -1,3 +1,27 @@
+class InvalidType < StandardError; end
+
 class State
-  attr_accessor :name, :url, :lottogames
+  attr_accessor :name, :url
+
+
+  def initialize #create an empty area of lottogames whenevr a new state is created
+    @lottogames = []
+  end
+
+  def lottogames
+    @lottogames.dup.freeze
+  end
+
+  def add_lottogame(lottogame)
+    if !lottogame.is_a?(LottoGame)
+      raise InvalidType, "must be a LottoGame" #add title
+    else
+      @lottogames << lottogame
+    end
+  end
+
+
+  def first_game_title
+    self.lottogames.first.title
+  end
 end
