ccd940e766285f317735d269936d8ea2b6a2c310
Rebecca Strauss <rstrauss127@gmail.com>
2018-12-10 20:40:38 +0000

    deleted State.rb

--- lib/ny_lotto_scraper.rb
+++ lib/ny_lotto_scraper.rb
@@ -6,7 +6,7 @@ require 'require_all'
 require_all "./lib"
 require_relative './ny_lotto_scraper/cli'
 require_relative './ny_lotto_scraper/game'
-require_relative './ny_lotto_scraper/state'
+
 
 
 require 'bundler'
